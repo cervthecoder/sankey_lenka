3a205e303b1f2e464b395c5c5012920ffd66173f
Matěj Červenka <matej.cervenka03@gmail.com>
2020-07-31 09:29:30 +0200

    Update: made changes and removed bugs

--- funcs.py
+++ funcs.py
@@ -2,14 +2,14 @@ import plotly.graph_objects as go
 import pandas as pd
 
 
-def make_fig(labels, source, target, value, diagram_name):
+def make_fig(labels, source, target, value, colors, diagram_name):
     fig = go.Figure(data=[go.Sankey(
     node = dict(
       pad = 15,
       thickness = 20,
       line = dict(color = "black", width = 0.5),
       label = labels,
-      color = dict(colors)
+      color = colors
     ),
     link = dict(
       source = source,
@@ -24,4 +24,4 @@ def get_data(csv_file):
     data = pd.read_csv(f"data/{csv_file}")
     df = data.drop("labels", 1)
     labels = [col for col in data.columns if col != "labels"]
-    return df.values.tolist() + labels # source - target - value - nan - color
+    return df.values.tolist() + [labels] # source - target - value - nan - color
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from funcs import get_data, make_fig
 
 
 if __name__ == '__main__':
-    source, target, value, nan, color, labels = get_data(input('Zadej jméno souboru ve formátu "xxx.csv": '))
-    make_fig(labels, source, target, value, color)
+    source, target, value, na, color, labels = get_data(input('Zadej jméno souboru ve formátu xxx.csv : '))
+    dia_name = input('Zadej jméno diagramu : ')
+    make_fig(labels, source, target, value, color, dia_name)
 
